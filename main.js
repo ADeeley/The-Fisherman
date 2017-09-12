1c4a7de51824d63d37005ba0785b9cdf565b9536
Adam Deeley <ad.deeley@gmail.com>
2017-09-12 21:43:33 +0100

    Added boat movement

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var canvas     = document.getElementById("myCanvas");
+var ctx        = canvas.getContext("2d");
 
 window.addEventListener("keydown", eventHandler, false);
 
 var keys = {
-    SPACE : 32,
+    SPACE     : 32,
+    A_KEY     : 65,
+    D_Key     : 68,
+    LEFT_KEY  : 37,
+    RIGHT_KEY : 39
 }
 
+// Gradient variable for the ocean
 var gradient = ctx.createLinearGradient(0, canvas.height/2 ,0, 500); 
 gradient.addColorStop(0, "#1658EA");
 gradient.addColorStop(1, "black");
@@ -14,22 +19,25 @@ gradient.addColorStop(1, "black");
 function Game() {
     this.startScreen = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.font = "40pt Ariel";
-        ctx.fillStyle = "#EEF2FB";
-        ctx.fillText("The Fisherman", canvas.height/2-140, 100);
         this.drawBackground();
+        this.drawTitle();
+        boat.draw();
     }
     this.gameLoop = function() {
+        this.drawBackground();
+        boat.draw();
     }
     this.deathScreen = function() {
         ctx.font = "40pt Ariel";
         ctx.fillStyle = "#c2c4ae";
         ctx.fillText("You died!", canvas.width/4, canvas.height/4);
+        this.drawBackground();
     }
     this.victory = function() {
         ctx.font = "40pt Ariel";
         ctx.fillStyle = "#c2c4ae";
         ctx.fillText("You won!", canvas.width/4, canvas.height/4);
+        this.drawBackground();
     }
     this.drawBackground = function() {
         ctx.beginPath();
@@ -37,6 +45,12 @@ function Game() {
         ctx.fillStyle = gradient;
         ctx.fill();
         ctx.closePath();
+    }
+    this.drawTitle = function() {
+        ctx.font = "40pt Ariel";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText("The", 20, canvas.height/2 - 5);
+        ctx.fillText("Fisherman", 20, (canvas.height/2) + 40);
     }
 }
 
@@ -74,11 +88,15 @@ function eventHandler(e) {
     if (stateHandler.startScreen) {
 
         if (e.keyCode == keys.SPACE) {
+            stateHandler.startGame();
         }
     }
     else if (stateHandler.gameLoop) {
-        if (e.keyCode == keys.SPACE) {
-
+        if (e.keyCode == keys.A_KEY) {
+            boat.moveLeft();
+        }
+        else if (e.keyCode == keys.D_Key) {
+            boat.moveRight();
         }
     }
     else if (stateHandler.deathSequence) {
@@ -87,14 +105,41 @@ function eventHandler(e) {
     }
 }
 
+function Boat() {
+    this.x         = canvas.width/2;
+    this.y         = canvas.height/2;
+    this.height    = 30;
+    this.speed     = 3;
+
+       
+    //Boat sprite setup
+    this.boatSprite = new Image();
+    this.boatSprite.src = "boat.bmp";
+
+    this.draw = function() {
+        ctx.drawImage(this.boatSprite, this.x, this.y - this.height);
+    }
+    
+    this.moveLeft = function() {
+        this.x -= this.speed;
+        console.log("left");
+    }
+    this.moveRight = function() {
+        this.x += this.speed;
+        console.log("right");
+    }
+}
+
 // Object assignments to variables
 var stateHandler;
 var game;
+var boat;
 
 function setup() {
     // Object instantiations
     stateHandler = new StateHandler();
     game         = new Game();
+    boat         = new Boat();
 
 }
 
@@ -105,7 +150,6 @@ function draw() {
     }
     else if (stateHandler.gameLoop) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        console.log("here");
         game.gameLoop();
     }
     else if (stateHandler.dead) {
